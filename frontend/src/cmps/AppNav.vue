7b410cfcfdf44cf01c59f334e6e6a3aad63a47f2
rania <raniakittan@hotmail.com>
2023-03-21 19:59:50 +0200

    grid layout

--- frontend/src/cmps/AppNav.vue
+++ frontend/src/cmps/AppNav.vue
@@ -1,12 +1,19 @@
 <template>
   <nav>
+    <div class="logo-container">
+    <img src="https://www.citypng.com/public/uploads/small/11661570403whwxybsmx9s49gydvycyvydsof4sqok0xk0cisomyqi9tvzojomgsfwp1ffra1pqt5ndii64wnwpzmkqsvjr7wkuh3fpukvy2eki.png" alt="Muzikay logo" class="logo">
     <span>Muzikay</span>
+  </div>
+  <div class="main-nav">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/search">Search</RouterLink>
     <RouterLink to="/library">Your Library</RouterLink>
+  </div>
+
+  <div class="liked-create-nav">
     <RouterLink to="/create">Create playlist</RouterLink>
     <RouterLink to="/like">Liked songs</RouterLink>
-    <RouterLink to="/cookies">cookies</RouterLink>
+  </div>
   </nav>
 </template>
 <script>
