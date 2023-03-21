70c07402429290a55bef833928fb66aefc70be85
Your Name <youremail@example.com>
2023-03-22 01:26:37 +0200

    station-details

--- frontend/src/views/StationDetails.vue
+++ frontend/src/views/StationDetails.vue
@@ -1,20 +1,25 @@
 <template>
+
   <section v-if="station" class="station-details">
     <section class="stetion">
       <h1>Playlist</h1>
       <p>Playlist Relax and indulge with beautiful piano pieces</p>
-      <h1>{{ station.name }}</h1>
-      <img :src="station.songs[0].imgUrl" />
+      <h1>{{ station.name || stationCount}}</h1>
+      <img v-if="station.songs" :src="station.songs[0].imgUrl" />
+      <button v-else><svg>🎵</svg></button>
       <img
         class="spotify-img"
         src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
       />
-      <span>6,950,331 likes</span>, <span>{{ songsCount }}</span
-      >,
-      <span>about 11 hr </span>
+      <div v-if="station.songs">
+        <span >6,950,331 likes</span>, <span>{{ songsCount }}</span>
+        <span>about 11 hr </span>
+      </div>
+      <div v-else>...</div>
     </section>
     <hr />
-    <ul class="clean-list">
+
+    <ul v-if="station.songs" class="clean-list">
       <li class="station" v-for="(song, idx) in station.songs" :key="idx">
         <span>{{ idx + 1 }}</span>
         <img :src="song.imgUrl" />
@@ -26,6 +31,14 @@
         <!-- </div> -->
       </li>
     </ul>
+    <section v-else class="empty-station-content">
+      <button>x</button>
+      <div>
+        <h3>Let's find something for your playlist</h3>
+        <input type="text" placeholder="Search for songs or episodes">
+      </div>
+    </section>
+
   </section>
 </template>
 
@@ -37,6 +50,7 @@ export default {
   data() {
     return {
       station: null,
+      counter: 0,
     }
   },
   watch: {
@@ -56,6 +70,12 @@ export default {
       const count = this.station.songs.length
       return `${count} Songs`
     },
+    stationCount() {
+      this.counter++
+      // const count = this.station.length
+      return `My Playlist #${this.counter}`
+    },
+
   },
 }
 </script>
