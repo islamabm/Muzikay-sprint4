09a206b3165eac8d7c6fc398b5fcaecf7e79f2a6
Your Name <youremail@example.com>
2023-03-21 18:55:06 +0200

    fix routs

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,43 +6,58 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // import ReviewIndex from './views/ReviewIndex.vue'
 // import LoginSignup from './views/LoginSignup.vue'
 // import UserDetails from './views/UserDetails.vue'
+
 import Search from './views/Search.vue'
 import UserLibrary from './views/UserLibrary.vue'
 import CreateStation from './views/CreateStation.vue'
 import LikedSongs from './views/LikedSongs.vue'
-import Cookies from './views/Cookies.vue'
 import StationIndex from './views/StationIndex.vue'
+
+//  buttons for navigation
+
+{/* <RouterLink to="/back">back</RouterLink> */}
+{/* <RouterLink to="/go">go</RouterLink> */}
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: StationIndex,
+    
+    childern: [
+      {
+        path: '/search',
+        name: 'search',
+        component: Search,
+      },
+      {
+        path: '/library',
+        name: 'Library',
+        component: UserLibrary,
+      },
+      {
+        path: '/create',
+        name: 'CreateStation',
+        component: CreateStation,
+      },
+      {
+        path: '/like',
+        name: 'like',
+        component: LikedSongs,
+      },
+    ],
+    
   },
   {
-    path: '/search',
-    name: 'search',
-    component: Search,
+    path: '/signup',
+    name: 'signup',
+    component: Signup,
   },
   {
-    path: '/library',
-    name: 'Library',
-    component: UserLibrary,
-  },
-  {
-    path: '/create',
-    name: 'CreateStation',
-    component: CreateStation,
-  },
-  {
-    path: '/like',
-    name: 'like',
-    component: LikedSongs,
-  },
-  {
-    path: '/cookies',
-    name: 'cookies',
-    component: Cookies,
+    path: '/login',
+    name: 'login',
+    component: Login,
   },
 ]
 
