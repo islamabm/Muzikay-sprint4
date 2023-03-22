1b49e6d89fc1e1874641dadd6ffff0be3b03588d
Your Name <youremail@example.com>
2023-03-22 11:26:39 +0200

    footer

--- frontend/src/cmps/FooterPlayer.vue
+++ frontend/src/cmps/FooterPlayer.vue
@@ -1,23 +1,57 @@
 <template>
-    <YouTube 
-        src="https://www.youtube.com/watch?v=jNQXAC9IVRw" 
-        @ready="onReady"
-        ref="youtube" />
+<div>
+    <vue-youtube
+      ref="youtubePlayer"
+      video-id="RjrA-slMoZ4"
+      :player-vars="playerVars"
+      @ready="onPlayerReady"
+      @state-change="onPlayerStateChange"
+    />
+    <button @click="playAudio">Play</button>
+    <button @click="pauseAudio">Pause</button>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-import YouTube from 'vue3-youtube'
 
-export default defineComponent({
-    components: { YouTube },
-    methods: {
-        onReady() {
-            console.log(this.$refs.youtube);
-            this.$refs.youtube.getCurrentTime()
-        },
+<script>
+export default {
+  data() {
+    return {
+      player: null,
+      audio: new Audio(),
+      playerVars: {
+        autoplay: 0,
+        mute: 1,
+        controls: 0,
+        showinfo: 0,
+        rel: 0,
+        fs: 0,
+      },
+    }
+  },
+  methods: {
+    onPlayerReady(event) {
+      this.player = event.target
+      this.audio.src = `https://www.youtube.com/watch?v=${this.player.getVideoData().video_id}`
+      this.audio.load()
     },
-})
+    onPlayerStateChange(event) {
+      if (event.data === window.YT.PlayerState.PLAYING) {
+        setInterval(() => {
+          this.audio.currentTime = this.player.getCurrentTime()
+        }, 1000)
+      }
+    },
+    playAudio() {
+      this.player.playVideo()
+      this.audio.play()
+    },
+    pauseAudio() {
+      this.player.pauseVideo()
+      this.audio.pause()
+    },
+  },
+}
 </script>
 <!-- 
 <template>
