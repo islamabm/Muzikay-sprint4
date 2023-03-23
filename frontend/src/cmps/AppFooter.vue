8f5de41022f4c70ca76bdcf226709ca0fbcf89bd
Your Name <youremail@example.com>
2023-03-23 10:14:35 +0200

    preeting code

--- frontend/src/cmps/AppFooter.vue
+++ frontend/src/cmps/AppFooter.vue
@@ -1,13 +1,8 @@
 <template>
   <footer class="main-footer">
     <div v-if="station" class="footer-details">
-<<<<<<< HEAD
       <img class="footer-details-img" :src="station.songs[0].imgUrl">
       <h3>{{ station.songs[0].title }}</h3>
-=======
-      <img class="footer-details-img" :src="station[0].imgUrl" />
-      <h3>{{ station[0].title }}</h3>
->>>>>>> 635248b7ee1704d979a8ae3c76cf9223b52ad0cb
       <button>💚</button>
       <button>🖼</button>
     </div>
@@ -28,11 +23,7 @@ export default {
   name: 'AppFooter',
   data() {
     return {
-<<<<<<< HEAD
       station: null
-=======
-      station: null,
->>>>>>> 635248b7ee1704d979a8ae3c76cf9223b52ad0cb
     }
   },
   created() {
@@ -45,17 +36,10 @@ export default {
         const station = await stationService.getById(stationId)
         console.log(station);
         try {
-<<<<<<< HEAD
           this.station = station
         }
         catch (err) {
           console.log(err);
-=======
-          this.station = station.songs
-          return this.station
-        } catch (err) {
-          console.log(err)
->>>>>>> 635248b7ee1704d979a8ae3c76cf9223b52ad0cb
         }
       },
       immediate: true,
