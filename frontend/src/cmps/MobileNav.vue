a9c5b993a301a5698bc2e7215b97f3b567bf3b48
rania <raniakittan@hotmail.com>
2023-03-26 15:25:47 +0300

    style for media query nav mobile

--- frontend/src/cmps/MobileNav.vue
+++ frontend/src/cmps/MobileNav.vue
@@ -1,20 +1,19 @@
 <template>
   <article class="mobile-nav">
-          <RouterLink class="router-link" to="/station/collection">
-            <i class="home-icon icons" v-html="getSvg('homeIcon')"></i>
-          </RouterLink>
+    <RouterLink class="router-link" to="/station/collection">
+      <i class="home-icon icons" :class="{ activeLink: $route.path === '/station/collection' }" v-html="getSvg('homeIcon')"></i>
+    </RouterLink>
 
+    <RouterLink class="router-link" to="/station/search">
+      <i class="search-icon icons" :class="{ activeLink: $route.path === '/station/search' }" v-html="getSvg('searchIcon')"></i>
+    </RouterLink>
 
-        <RouterLink class="router-link"  to="/station/search">
-          <i class="search-icon icons" v-html="getSvg('searchIcon')"></i>
-        </RouterLink>
-
-        <RouterLink class="router-link"  to="/station/library">
-          <i class="library-icon icons" v-html="getSvg('libraryIcon')"></i>
-        </RouterLink>
-
+    <RouterLink class="router-link" to="/station/library">
+      <i class="library-icon icons" :class="{ activeLink: $route.path === '/station/library' }" v-html="getSvg('libraryIcon')"></i>
+    </RouterLink>
   </article>
 </template>
+
 
 <script>
 import svgService from '../services/SVG.service.js'
