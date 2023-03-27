7059b1092d3b0b0c6398e6fedbcae7f40a2761cb
Your Name <youremail@example.com>
2023-03-27 13:31:31 +0300

    media player css

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,14 +2,12 @@ import Vuex from 'vuex'
 
 import { userStore } from './user.store.js'
 import { stationStore } from './station.store.js'
-import { MediaPlayerStore } from './mediaPlayer.store.js'
 
 export const store = Vuex.createStore({
   strict: true,
   modules: {
     userStore,
     stationStore,
-    MediaPlayerStore,
   },
   state: {},
   mutations: {},
