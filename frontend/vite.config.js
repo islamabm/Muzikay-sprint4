bc2dfc077061d4dc9298d4f80134a5d653fd2ddc
rania <raniakittan@hotmail.com>
2023-03-31 18:47:57 +0300

    pulling conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,14 +6,8 @@ export default defineConfig({
   plugins: [vue()],
 
   build: {
-<<<<<<< HEAD
     outDir: '../muzikay-backend/public',
     emptyOutDir: true
   }
-=======
-    outDir: '../backend/public',
-    emptyOutDir: true,
-  },
->>>>>>> ce5dce4ca34a3038ca53c5cc57b5eaf0d56caf0e
 })
 
