23a1118ed8713a3e98cce3e78ac96910fd028cb4
islam abo mokh <alo.jalal.55@gmail.com>
2023-04-01 23:41:47 +0300

    conflict

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,31 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="container">
-    <div class="header">
-      <h1 class="title">Chatify</h1>
-      <p class="subtitle">
-        Discuss your favorite music genres with others in real-time, discover
-        new artists and expand your musical horizons with Chatify.
-      </p>
-    </div>
-    <h3 v-if="typingUser">{{ typingUser }} is typing...</h3>
-    <h2 v-if="topic === 'General'">Let's Talk About Music</h2>
-    <h2 v-else>Lets Talk About {{ topic }} Music</h2>
-    <label for="topic-select">Choose a topic:</label>
-    <select id="topic-select" v-model="topic" @change="changeTopic">
-      <option v-for="t in topics" :value="t">{{ t }}</option>
-    </select>
-    <ul>
-      <li v-for="(msg, idx) in msgs" :key="idx">
-        <span>{{ msg.from }}:</span>{{ msg.txt }}
-      </li>
-    </ul>
-    <hr />
-    <form @submit.prevent="sendMsg">
-      <input type="text" v-model="msg.txt" placeholder="Your msg" />
-      <button>Send</button>
-    </form>
-=======
   <div class="chat-room-container">
     <h2>Chat</h2>
     <h3 v-if="typingUser">{{ typingUser }} is typing...</h3>
@@ -47,7 +20,6 @@
       />
       <button @click="sendMsg" class="btn">Send</button>
     </div>
->>>>>>> 127ded61a8d51e3f980839caa293f49254391a41
   </div>
 </template>
 <script>
@@ -71,28 +43,6 @@ export default {
     return {
       msgTxt: '',
       msgs: [],
-<<<<<<< HEAD
-      topics: [
-        'General',
-        'Mood',
-        'Rock',
-        'Pop',
-        'Hip-Hop',
-        'Focus',
-        'Alternative',
-        'Indie',
-        'Sleep',
-        'Decades',
-        'Workout',
-        'At Home',
-        'Travel',
-        'Chill',
-        'Party',
-        'Dance/Electronics',
-      ],
-      topic: 'General',
-=======
->>>>>>> 127ded61a8d51e3f980839caa293f49254391a41
       typingUser: '',
     }
   },
